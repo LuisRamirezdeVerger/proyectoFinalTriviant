0bc26d25f816a2621f80f44db8330bd993a09c1a
Luis a.k.a. Wito <lrl0015@alu.medac.es>
2025-01-18 23:17:30 +0100

    player.java finished, need to add skills

--- app/src/main/java/com/example/triviant/Player.java
+++ app/src/main/java/com/example/triviant/Player.java
@@ -1,4 +1,36 @@
 package com.example.triviant;
 
 public class Player {
+    private String name;
+    private int position = 0;
+    private boolean skipTurn = false;
+
+    public Player (String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public void setPosition(int position){
+        this.position = position;
+    }
+
+    public void move(int steps){
+        position =+ steps;
+    }
+
+    public boolean isSkipTurn(){
+        return skipTurn;
+    }
+
+    public void setSkipTurn(boolean skipTurn){
+        this.skipTurn = skipTurn;
+    }
+    //We've to add skills to every character!!
 }
