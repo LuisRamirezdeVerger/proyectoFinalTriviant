aab3ab39f80ebf3fae703de45415573631ba1d61
Adrián Lama González <adrianlama34@gmail.com>
2025-01-20 10:57:54 +0100

    Data Binding y Random
    El botón no se ve en el emulador

--- app/src/main/java/com/example/triviant/MainActivity.java
+++ app/src/main/java/com/example/triviant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.triviant;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.*;
+
+import com.example.triviant.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Testing
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding.buttonCard.setOnClickListener(v -> {
+            Random random = new Random(5);
+            int movement = random.nextInt();
+            System.out.println(movement);
+            //openOtherclass();
+        });
     }
+/*
+    private void openLogin() {
+        Intent intent = new Intent(this, Otherclass.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
+    }
+*/
+
 }
