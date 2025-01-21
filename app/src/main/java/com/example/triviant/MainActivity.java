93fabff44dfa0d979369f3247243683ff7decdd3
unknown <lrl0015@alu.medac.es>
2025-01-21 14:37:58 +0100

    Firebase added, JSON with questions, too

--- app/src/main/java/com/example/triviant/MainActivity.java
+++ app/src/main/java/com/example/triviant/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // Llamar al method para escribir datos
+        FirebaseWriteTest firebaseWriteTest = new FirebaseWriteTest();
+        firebaseWriteTest.writeData();
         //setContentView(R.layout.activity_main);
        // Testing
     }
