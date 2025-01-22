8bcfab55b7853283cd4f06ba5028bffd888ccfcb
unknown <lrl0015@alu.medac.es>
2025-01-22 13:36:56 +0100

    GameUtils files & diceRoll added

---
+++ app/src/main/java/com/example/triviant/GameUtils.java
@@ -0,0 +1,11 @@
+package com.example.triviant;
+
+import java.util.*;
+
+public class GameUtils {
+
+    public static int diceRoll (){
+        Random dice = new Random();
+        return dice.nextInt(6) + 1;
+    }
+}
