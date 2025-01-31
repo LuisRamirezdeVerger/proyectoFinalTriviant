7b881e7d78cf5e28a9e071ac9a94f9670443dcf0
Adrián Lama González <adrianlama34@gmail.com>
2025-01-31 12:41:19 +0100

    Crear o unirse

--- app/src/main/java/com/example/triviant/CreateJoin.java
+++ app/src/main/java/com/example/triviant/CreateJoin.java
@@ -1,6 +1,8 @@
 package com.example.triviant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,60 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CreateJoin extends AppCompatActivity {
 
+    ImageView buttonCreate = findViewById(R.id.ButtonCreate);
+    ImageView buttonJoin = findViewById(R.id.ButtonJoin);
+    ImageView buttonSettings = findViewById(R.id.settings);
+    ImageView buttonBack = findViewById(R.id.back);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_join);
 
+        buttonSettings.setOnClickListener(v ->
+        {
+            openSettings();
+        });
+
+        buttonCreate.setOnClickListener(v ->
+        {
+            openCreate();
+        });
+
+        buttonJoin.setOnClickListener(v ->
+        {
+            openJoin();
+        });
+
+        buttonBack.setOnClickListener(v ->
+        {
+            openBack();
+        });
+    }
+
+    void openSettings() {
+        Intent intent = new Intent(this, Settings.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
+    }
+
+    void openCreate() {
+        Intent intent = new Intent(this, CreateRoom.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
+    }
+
+    void openJoin() {
+        Intent intent = new Intent(this, JoinRoom.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
+    }
+
+    void openBack() {
+        Intent intent = new Intent(this, Title.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
     }
 }
+
--- app/src/main/java/com/example/triviant/CreateRoom.java
+++ app/src/main/java/com/example/triviant/CreateRoom.java
@@ -1,6 +1,8 @@
 package com.example.triviant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CreateRoom extends AppCompatActivity {
 
+    ImageView buttonBack = findViewById(R.id.back);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_room);
 
+
+        buttonBack.setOnClickListener(v ->
+        {
+            openBack();
+        });
+    }
+
+    void openBack() {
+        Intent intent = new Intent(this, Title.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
     }
 }
