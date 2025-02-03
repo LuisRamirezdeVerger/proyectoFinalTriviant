0eeff2a80e2cdc1dfd9dd476578f789c1681a634
Adrián Lama González <adrianlama34@gmail.com>
2025-02-03 12:35:13 +0100

    Settings y selección de idioma

--- app/src/main/java/com/example/triviant/Settings.java
+++ app/src/main/java/com/example/triviant/Settings.java
@@ -1,6 +1,9 @@
 package com.example.triviant;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class Settings extends AppCompatActivity {
+
+    ImageView Spanish = findViewById(R.id.spain);
+    ImageView English = findViewById(R.id.uk);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Spanish.setOnClickListener(v -> changeLanguage(new Locale("es")));
+        English.setOnClickListener(v -> changeLanguage(new Locale("en")));
+
     }
+
+    private void changeLanguage(Locale newLocale) {
+        Locale.setDefault(newLocale);
+        Configuration config = new Configuration();
+        config.locale = newLocale;
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        recreate();
+    }
+
 }
