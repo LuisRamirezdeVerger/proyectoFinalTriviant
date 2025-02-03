1732bed6032128aee516b5e8665b944ae054fb39
Adrián Lama González <adrianlama34@gmail.com>
2025-02-03 13:24:24 +0100

    Sliders finalizados

--- app/src/main/java/com/example/triviant/Settings.java
+++ app/src/main/java/com/example/triviant/Settings.java
@@ -1,6 +1,7 @@
 package com.example.triviant;
 
 import android.content.res.Configuration;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,12 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.slider.Slider;
+
 import java.util.Locale;
 
 public class Settings extends AppCompatActivity {
 
     ImageView Spanish = findViewById(R.id.spain);
     ImageView English = findViewById(R.id.uk);
+    Slider Music = findViewById(R.id.SliderMusic);
+    Slider Volume = findViewById(R.id.SliderVolume);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,8 @@ public class Settings extends AppCompatActivity {
 
         Spanish.setOnClickListener(v -> changeLanguage(new Locale("es")));
         English.setOnClickListener(v -> changeLanguage(new Locale("en")));
+        Music.addOnChangeListener((slider, value, fromUser) -> adjustMusicVolume(value));
+        Volume.addOnChangeListener((slider, value, fromUser) -> adjustGeneralVolume(value));
 
     }
 
@@ -35,6 +42,20 @@ public class Settings extends AppCompatActivity {
         config.locale = newLocale;
         getResources().updateConfiguration(config, getResources().getDisplayMetrics());
         recreate();
+    }
+
+    private void adjustMusicVolume(float value) {
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int newVolume = (int) (value * maxVolume / 100);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVolume, 0);
+    }
+
+    private void adjustGeneralVolume(float value) {
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM);
+        int newVolume = (int) (value * maxVolume / 100);
+        audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, newVolume, 0);
     }
 
 }
