56d86f74d9270821bbf850f9c4d659300b637ee2
Adrián Lama González <adrianlama34@gmail.com>
2025-02-07 14:20:39 +0100

    Bases de charinfo

--- app/src/main/java/com/example/triviant/CharSelect.java
+++ app/src/main/java/com/example/triviant/CharSelect.java
@@ -1,6 +1,8 @@
 package com.example.triviant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,43 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CharSelect extends AppCompatActivity {
 
+    ImageView buttonBack = findViewById(R.id.back);
+    ImageView buttonSmart = findViewById(R.id.smart);
+    ImageView buttonBuff = findViewById(R.id.buff);
+    ImageView buttonWizzard = findViewById(R.id.wizzard);
+    ImageView buttonZombie = findViewById(R.id.zombie);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_char_select);
 
+        buttonBack.setOnClickListener(v ->
+        {
+            openBack();
+        });
+        buttonSmart.setOnClickListener(v ->
+        {
+            openBack();
+        });
+        buttonBuff.setOnClickListener(v ->
+        {
+            openBack();
+        });
+        buttonWizzard.setOnClickListener(v ->
+        {
+            openBack();
+        });
+        buttonZombie.setOnClickListener(v ->
+        {
+            openBack();
+        });
+    }
+
+    void openBack() {
+        Intent intent = new Intent(this, WaitingRoom.class); // Crear el intent
+
+        startActivity(intent); // Lanzamos el intent
     }
 }
